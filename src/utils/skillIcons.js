9dce97795ebc0d2df757e8db089aa4e6470208d2
Arfion Rizki Diotama <arfion@noice.id>
2026-02-18 09:51:26 +0700

    fix: Update Locust skill icon URL for Locust.

--- src/utils/skillIcons.js
+++ src/utils/skillIcons.js
@@ -18,7 +18,8 @@ const skillIcons = {
   Swagger: `${DEVICON}/swagger/swagger-original.svg`,
 
   // Performance Testing
-  Locust: 'https://locust.io/static/img/logo.png',
+  Locust:
+    'https://substackcdn.com/image/fetch/$s_!8EZ-!,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F87c041a3-9425-4909-95a8-5db4ec5c8b3c_600x604.png',
   Grafana: `${DEVICON}/grafana/grafana-original.svg`,
 
   // Tools & Bug Tracking
